cdc342b41d0a18adb1b65deaec7dbfac80f4216a
Pratim Majumder <ExploiTR@users.noreply.github.com>
2023-10-30 21:44:43 +0530

    ok

---
+++ udemy_c++/charstr/char_arr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+// #include <gmp.h>
+using namespace std;
+
+
+int main(){
+    freopen("C:\\Users\\prati\\OneDrive\\Documents\\GitHub\\DSA_Udemy\\udemy_c++\\input.txt","r",stdin);
+    freopen("C:\\Users\\prati\\OneDrive\\Documents\\GitHub\\DSA_Udemy\\udemy_c++\\output.txt","w",stdout);
+
+    /*
+    * 
+    */
+
+    char arr[5] = {'a','b','c','d','\0'}; 
+    char * arr = "ss";
+
+    cout << arr;
+
+    /*
+    * Now if you want to get a line input with space
+    * like read all till $ symbol triggered
+    */
+
+    cin.get();
+
+    return 0;
+}
