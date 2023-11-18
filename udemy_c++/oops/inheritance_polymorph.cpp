ba8e6858528ecca2882718291d08d7f838bab6ce
Pratim Majumder <ExploiTR@users.noreply.github.com>
2023-11-18 23:29:52 +0530

    ok

---
+++ udemy_c++/oops/inheritance_polymorph.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <gmp.h>
+using namespace std;
+
+
+int main(){
+    int x,y;
+
+    freopen("C:\\Users\\prati\\OneDrive\\Documents\\GitHub\\DSA_Udemy\\udemy_c++\\input.txt","r",stdin);
+    freopen("C:\\Users\\prati\\OneDrive\\Documents\\GitHub\\DSA_Udemy\\udemy_c++\\output.txt","w",stdout);
+
+    cin >> x >> y;
+    cout << x + y <<endl;
+
+    return 0;
+}
--- udemy_c++/oops/oops_start.cpp
+++ udemy_c++/oops/oops_start.cpp
@@ -5,37 +5,68 @@ using namespace std;
 
 
 class Player{
+
     private:
-        string fName;
-        string lName;
+        const string fName; //constant member
+        const string lName;
         string handle;
-        int guess;
+        int * checkPtr;
 
     public:
         Player(){
-
         }
 
-        Player(string fName, string lName, string handle){
-            this->fName = fName;
-            this->lName = lName;
+        //Shallow copy constructor -
+        // Player(Player &p) : fName(p.fName), lName(p.lName) {
+        //     this->handle = p.handle;
+        //     this->checkPtr = p.checkPtr;
+        // }
+
+        //Deep copy constructor -
+        Player(Player &p) : fName(p.fName), lName(p.lName) {
+            this->handle = p.handle;
+            this->checkPtr = new int[1];
+            this->checkPtr[0] = 0; //avoid garbage value
+        }
+
+        //dealing with constant members 
+        Player(string f_name, string l_name, string handle) : fName(f_name), lName(l_name) {
+            checkPtr = new int[1];
             this->handle = handle;
         }
 
-        void displayName(){
-            cout << "Name : " << fName << " " << lName;
+        void displayName() const{
+            cout << "Name : " << fName << " " << lName << endl;
         }
 
-        void setName(string fName, string lName){
-            this->fName = fName;
-            this->lName = lName;
+        void setHandle(string handle){ 
+            this->handle = handle;
         }
 
-        void display(){
-            cout << handle << " has guessed : " << guess << endl;
+        string getHandle() const{
+            return this->handle;
+        }
+
+        void makeGuess(){
+            checkPtr[0] = rand()%10;
+        }
+
+        void display() const{
+            cout << handle << " has guessed : " << checkPtr[0] << endl;
+        }
+
+        //operator overloading || copy assignment operator
+        void operator=(Player &p){
+            //const operators cant be copied!
+        }
+
+        //Desh-truhct-teurh
+        ~Player(){
+            if(checkPtr != NULL)
+                delete [] checkPtr;
+            cout << "Destroying : " << handle << endl;
         }
 };
-
 
 
 int main(){
@@ -47,12 +78,27 @@ int main(){
     // each player has to make a random guess between 1-10
     // correct guess wins!
 
-    //Player knows : fname,sname,handle,guess
-    //Player can : makeGuess(), changeHandle()
+    // Player knows : fname,sname,handle,guess
+    // Player can : makeGuess(), changeHandle()
 
-    Player p1;
-    p1.setName("Pratim","Majumder");
-    p1.displayName();
+    Player p1("Pratim","Majumder", "pm1");
+    Player p2(p1); //Default copy const : C++ feature.
+
+    p1.makeGuess();
+    p2.makeGuess();
+
+    p1.display();
+    p2.display();
+
+    //this -> : this is a pointer to current obj, so we can try below
+
+    Player * p3 = new Player("E","C", "ecx");
+    p3-> makeGuess();
+    p3 -> display();
+
+    // no destrcutor called for dynamically allocated objetcs on heap unless calle delete
+
+    delete p3;
 
     return 0;
 }
