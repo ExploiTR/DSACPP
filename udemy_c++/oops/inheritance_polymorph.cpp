7871cfd92af54bd8f06f1d78ddf99756c5218704
Pratim Majumder <ExploiTR@users.noreply.github.com>
2023-11-19 00:58:15 +0530

    ok

--- udemy_c++/oops/inheritance_polymorph.cpp
+++ udemy_c++/oops/inheritance_polymorph.cpp
@@ -2,6 +2,53 @@
 // #include <gmp.h>
 using namespace std;
 
+class User{
+    public:
+        string name;
+        long id;
+
+        User(string name, long id){
+            this->name = name;
+            this->id = id;
+        }
+
+        void disp(){
+            cout << name << " -> " << id << endl;
+        }
+};
+
+/*
+* Public Inheritance : 
+* - Public -> Public
+* - Private -> No access
+* - Protected -> Protected
+*/
+class Employee : public User{
+
+    public:
+        int score;
+
+        Employee(string name, long id, int score) : User(name,id){
+            this->score = score;
+        }
+
+        void disp(bool unused){
+            User::disp();
+            cout << "Score : " << score << endl;
+        }
+
+};
+
+/*
+* Public Inheritance : 
+* - Public -> Private
+* - Private -> No access
+* - Protected -> Private
+*/
+class Employee2 : private User{
+
+};
+
 
 int main(){
     int x,y;
@@ -11,6 +58,9 @@ int main(){
 
     cin >> x >> y;
     cout << x + y <<endl;
+
+    Employee employee("meow",122345L,12);
+    employee.disp(true);
 
     return 0;
 }
