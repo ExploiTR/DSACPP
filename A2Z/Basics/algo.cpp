2115767062a1f81674872dc3cc36bef057e73f0a
Pratim Majumder <ExploiTR@users.noreply.github.com>
2024-04-02 13:57:01 +0530

    ok

--- A2Z/Basics/algo.cpp
+++ A2Z/Basics/algo.cpp
@@ -1,8 +1,22 @@
 #include "../header.h"
 
-int main(){
+int main()
+{
     iosetup();
 
-    //LOL
+    // comparator
+
+    // if the conditon is what u want return true or return false for a swap
+
+    int cnt = __builtin_popcount(7);
+    long long num = 7347843827348724353;
+    int cnt2 = __builtin_popcountll(num);
+
+    cout << cnt << endl
+         << cnt2 << endl;
+
+    //max_element(); 
+    //min_element
+    //next_permuation()
     return 0;
 }
