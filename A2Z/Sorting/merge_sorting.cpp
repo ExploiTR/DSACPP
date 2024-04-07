b086e90c22830aa26f915cd9dbc8c09799a46af5
Pratim Majumder <ExploiTR@users.noreply.github.com>
2024-04-07 17:22:24 +0530

    alr

--- A2Z/Sorting/merge_sorting.cpp
+++ A2Z/Sorting/merge_sorting.cpp
@@ -1,8 +1,35 @@
 #include "../header.h"
 
+void merge2(int arr[], int low, int mid, int high)
+{
+    int *temp = new int[high - low + 1]();
+
+    int left = low;
+    int right = mid + 1;
+    int cur_wp = 0;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] < arr[right])
+            temp[cur_wp++] = arr[left++];
+        else
+            temp[cur_wp++] = arr[right++];
+    }
+
+    while (left <= mid)
+        temp[cur_wp++] = arr[left++];
+
+    while (right <= high)
+        temp[cur_wp++] = arr[right++];
+
+    for (int i = low; i <= high; i++)
+        arr[i] = temp[i - low];
+
+    delete temp;
+}
+
 void merge(int arr[], int low, int mid, int high)
 {
-    // int *temp = new int[high - low + 1]();
     vector<int> temp;
 
     int left = low;
@@ -11,21 +38,10 @@ void merge(int arr[], int low, int mid, int high)
     while (left <= mid && right <= high)
     {
         if (arr[left] < arr[right])
-        {
-            // temp[left - low] = arr[left];
             temp.push_back(arr[left++]);
-        }
         else
-        {
-            // temp[right - mid + 1] = arr[right];
             temp.push_back(arr[right++]);
-        }
     }
-
-    // for (int i = 0; i < high - low + 1; i++)
-    // {
-    //     arr[i + low] = temp[i];
-    // }
 
     while (left <= mid)
     {
@@ -39,8 +55,6 @@ void merge(int arr[], int low, int mid, int high)
 
     for (int i = low; i <= high; i++)
         arr[i] = temp[i - low];
-
-    // delete temp;
 }
 
 void merge_sort(int arr[], int low, int high)
@@ -52,7 +66,7 @@ void merge_sort(int arr[], int low, int high)
     merge_sort(arr, low, mid);
     merge_sort(arr, mid + 1, high);
 
-    merge(arr, low, mid, high);
+    merge2(arr, low, mid, high);
 }
 
 int main()
