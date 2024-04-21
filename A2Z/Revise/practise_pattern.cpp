8286346a46766410ad456b6a5d10474362e482e5
Pratim Majumder <ExploiTR@users.noreply.github.com>
2024-04-21 19:50:32 +0530

    oops

--- A2Z/Revise/practise_pattern.cpp
+++ A2Z/Revise/practise_pattern.cpp
@@ -324,11 +324,36 @@ void print_pattern19(int size)
 {
     cout << "Pattern 19" << endl;
 
+    for (int i = size; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * (size - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     for (int i = 1; i <= size; i++)
     {
-        for (char j = 'A' + (size - i); j < 'A' + size; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << j;
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * (size - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
         }
         cout << endl;
     }
@@ -338,14 +363,66 @@ void print_pattern19(int size)
 
 void print_pattern20(int size)
 {
+    cout << "Pattern 20" << endl;
+
+    for (int i = 1; i <= size; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * (size - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = size - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * (size - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
 }
 
 void print_pattern21(int size)
 {
+    cout << "Pattern 21" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 void print_pattern22(int size)
 {
+    
 }
 
 int main()
